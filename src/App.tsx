817061dce779b219ba7b063c53153388dca9b80f
LeitnerVeronika <s2010238045@students.fh-hagenberg.at>
2024-04-23 22:42:35 +0200

    feat: test custom node features

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useCallback, useState} from 'react';
+import ReactFlow, {
+    addEdge,
+    applyEdgeChanges,
+    applyNodeChanges,
+    Background,
+    Controls,
+    NodeTypes,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+import TextUpdaterNode from './nodes/TextUpdaterNode.tsx';
+import './styles/text-updater-node.css'
+
+
+
+const initialNodes = [
+    {
+        id: '1',
+        data: {label: 'Hello'},
+        position: {x: 0, y: 0},
+        type: 'input',
+    },
+    {
+        id: '2',
+        data: {label: 'World'},
+        position: {x: 100, y: 100},
+    },
+    {
+        id: 'node-1',
+        type: 'custom',
+        position: {x: 200, y: 200},
+        data: {value: 123}
+    },
+
+];
+
+const initialEdges = [
+    {id: '1-2', source: '1', target: '2', label: 'to the', type: 'step'},
+    // {id: '1-2', source: '2', target: 'node-1', label: 'to the', type: 'step'},
+
+];
+
+const nodeTypes: NodeTypes = {
+    custom: TextUpdaterNode,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const onNodesChange = useCallback(
+        (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+        [],
+    );
+    const onEdgesChange = useCallback(
+        (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        [],
+    );
+    const onConnect = useCallback(
+        (params) => setEdges((eds) => addEdge(params, eds)),
+        [],
+    );
+
+    return (
+        <div style={{height: '100vh', width: '100vw'}}>
+            <ReactFlow
+                nodes={nodes}
+                onNodesChange={onNodesChange}
+                edges={edges}
+                onEdgesChange={onEdgesChange}
+                nodeTypes={nodeTypes}
+                onConnect={onConnect}
+                fitView
+            >
+                <Background/>
+                <Controls/>
+            </ReactFlow>
+        </div>
+    );
 }
 
-export default App
+export default App;
