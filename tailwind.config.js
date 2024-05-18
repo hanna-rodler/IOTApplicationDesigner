b068e48337ce71b8b45765c45bee7ed5ec7b8862
harodler <haj.rodler@gmail.com>
2024-05-18 16:19:05 +0200

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#03a696",
+          DEFAULT: "#038C8C",
+        },
+        secondary: {
+          DEFAULT: "#025159",
+        },
+        gray: {
+          fieldBg: "#F9FAF9",
+          fieldBorder: "#E5E7EB",
+        },
+        sidebar: "#F4F4F4",
+        appBg: "#F8F9FB",
+        cardBg: "#FEFEFE", // card background
+        orange: {
+          edge: "#F4F4F4",
+        },
+      },
+      borderRadius: {
+        btn: "5px",
+        top: "5px 5px 0px 0px",
+      },
+    },
+  },
+  plugins: [],
+};
