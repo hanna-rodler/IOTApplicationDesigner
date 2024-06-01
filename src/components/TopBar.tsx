f53d998e0ac0a948433f5dfaa72b91ea18dd2b9c
harodler <haj.rodler@gmail.com>
2024-06-01 11:20:14 +0200

    chore: add home link to topBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { FaRegSquarePlus } from "react-icons/fa6";
+import { NavLink } from "react-router-dom";
 
-const TopBar = ({ onAddTab }) => {
-  return (
-      <div className="bg-secondary text-white font-bold text-[30px] h-[50px] flex justify-between items-center relative z-5">
-      <div className="flex items-center gap-2 ml-2">
-        <img src="./../../public/logo.png" className="w-[35px] h-[35px] mr-2 ml-2" />
-        <div>IoT Configuration</div>
-      </div>
-      <div className="flex items-center mr-2">
-        <FaRegSquarePlus className="w-[35px] h-[35px] cursor-pointer" onClick={onAddTab} />
-      </div>
-    </div>
-  );
+const TopBar = ({ onAddTab, addButton }) => {
+      return (
+        <div className="bg-secondary text-white font-bold text-[30px] h-[50px] flex justify-between items-center relative z-5">
+          <NavLink className="flex items-center gap-2 ml-2"  to="/">
+            <img src="./../../public/logo.png" className="w-[35px] h-[35px] mr-2 ml-2" />
+            <div>IoT Configuration</div>
+          </NavLink>
+          <div className="flex items-center mr-2">
+            <FaRegSquarePlus className="w-[35px] h-[35px] cursor-pointer" onClick={onAddTab} />
+          </div>
+        </div>
+      );
 };
 
 export default TopBar;
