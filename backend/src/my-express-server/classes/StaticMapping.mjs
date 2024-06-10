60b2df4eaef4f5594a402268ca1eb29110abde76
harodler <haj.rodler@gmail.com>
2024-06-10 22:16:51 +0200

    chore: refactor Mapping to new data structure

--- backend/src/my-express-server/classes/StaticMapping.mjs
+++ backend/src/my-express-server/classes/StaticMapping.mjs
@@ -1,17 +1,19 @@
 import { generateRandomString } from "../utils/utils.mjs";
 
 export default class StaticMapping {
-    constructor(mapping, reportTopic) {
-        this.nodeType = 'static';
-        this.message = mapping.message_mapping.message;
-        this.mapped_message = mapping.message_mapping.mapped_message;
-        this.qos = mapping.qos !== undefined ? mapping.qos : '';
-        this.retain = mapping.retain !== undefined ? mapping.retain : '';
+    constructor(mapping, reportTopic, id=undefined) {
+        this.data = {
+            nodeType: 'static',
+            mapping: mapping.message_mapping.message,
+            mapped_message: mapping.message_mapping.mapped_message,
+            qos: mapping.qos !== undefined ? mapping.qos : '',
+            retain: mapping.retain !== undefined ? mapping.retain : ''
+        }        
         this.type = 'mapping';
         this.position = {x: 100, y: 100};
         this.commandTopic = mapping.mapped_topic;
         this.reportTopic = reportTopic;
-        this.id = 'static_'+generateRandomString(10);
+        this.id = id !== undefined ? id : 'static_'+generateRandomString(10);
     }
 
     display(){
@@ -21,11 +23,13 @@ export default class StaticMapping {
     getMapping() {
         return {
             id: this.id,
-            nodeType: this.nodeType,
-            message: this.message,
-            mapped_message: this.mapped_message,
-            qos: this.qos,
-            retain: this.retain,
+            data: {
+                nodeType: this.data.nodeType,
+                message: this.data.message,
+                mapped_message: this.data.mapped_message,
+                qos: this.data.qos,
+                retain: this.data.retain,
+            },
             type: this.type,
             position: this.position
         };
--- backend/src/my-express-server/classes/ValueJsonMapping.mjs
+++ backend/src/my-express-server/classes/ValueJsonMapping.mjs
@@ -1,17 +1,28 @@
 import { generateRandomString } from "../utils/utils.mjs";
 
 export default class ValueJsonMapping {
-    constructor(mapping, nodeType, reportTopic) {
+    constructor2(mapping, nodeType, reportTopic) {
         this.nodeType = nodeType;
         this.mapping = mapping.mapping_template;
-        this.qos = mapping.qos !== undefined ? mapping.qos : '';
-        this.retain = mapping.retain !== undefined ? mapping.retain : '';
-        this.type = 'mapping';
-        this.position = {x: 100, y: 100};
         this.commandTopic = mapping.mapped_topic;
         this.reportTopic = reportTopic;
         this.id = this.nodeType + '_' + generateRandomString(11);
     }
+
+    constructor(mapping, nodeType, reportTopic, id = undefined) {
+        this.data = {
+            nodeType: nodeType,
+            mapping: mapping.mapping_template,
+            qos: mapping.qos !== undefined ? mapping.qos : '',
+            retain: mapping.retain !== undefined ? mapping.retain : ''
+        }        
+        this.commandTopic = mapping.mapped_topic;
+        this.type = 'mapping';
+        this.position = {x: 100, y: 100};
+        this.reportTopic = reportTopic;
+        this.id = id !== undefined ? id : 'static_'+generateRandomString(10);
+    }
+
 
     display(){
         console.log('value json Mapping', this);
@@ -20,10 +31,12 @@ export default class ValueJsonMapping {
     getMapping() {
         return {
             id: this.id,
-            nodeType: this.nodeType,
-            mapping: this.mapping,
-            qos: this.qos,
-            retain: this.retain,
+            data: {
+                nodeType: this.data.nodeType,
+                mapping: this.data.mapping,
+                qos: this.data.qos,
+                retain: this.data.retain,
+            },
             type: this.type,
             position: this.position
         };
